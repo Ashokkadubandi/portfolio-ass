afc9d0e30a7c49a97b0df14f701fc58b3417eaee
AshokKadubandi <velmaashok765@gmail.com>
2025-06-13 17:25:44 +0530

    deploy

--- js/script.js
+++ js/script.js
@@ -12,38 +12,7 @@ function loadContent(id,file){
     })
 }
 
-// function renderThreeModel() {
-//   const container = document.getElementById("three-container");
 
-//   const scene = new THREE.Scene();
-//   const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-//   const renderer = new THREE.WebGLRenderer({ alpha: true });
-
-//   renderer.setSize(container.clientWidth, container.clientHeight);
-//   container.appendChild(renderer.domElement);
-
-//   // A simple rotating cube
-//   const geometry = new THREE.BoxGeometry();
-//   const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
-//   const cube = new THREE.Mesh(geometry, material);
-//   scene.add(cube);
-
-//   // Lighting
-//   const light = new THREE.PointLight(0xffffff, 1, 100);
-//   light.position.set(10, 10, 10);
-//   scene.add(light);
-
-//   camera.position.z = 5;
-
-//   function animate() {
-//     requestAnimationFrame(animate);
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
-//     renderer.render(scene, camera);
-//   }
-
-//   animate();
-// }
 
 
 
